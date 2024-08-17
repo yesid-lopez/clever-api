8617726b27a034a62aaebc1f485e8733b1cc7a2a
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2024-08-17 13:33:38 +0200

    create services

---
+++ study_buddy/repositories/file_repository.py
@@ -0,0 +1,35 @@
+import json
+
+from bson import ObjectId
+
+from study_buddy.models.file import File
+from study_buddy.utils.database import file_collection
+
+
+def save(file: File):
+    new_file = file_collection.insert_one(json.loads(file.model_dump(exclude={"id"})))
+    return str(new_file.inserted_id)
+
+
+def find_by_name(name: str) -> File | None:
+    file = file_collection.find_one({"path": name})
+
+    if not file:
+        return None
+
+    return File(**file)
+
+
+def find_by_id(_id: str) -> File | None:
+    file = file_collection.find_one({"_id": ObjectId(_id)})
+    if not file:
+        return None
+
+    return File(**file)
+
+
+def update(file: File):
+    file_collection.update_one(
+        {"_id": ObjectId(file.id)},
+        {"$set": json.loads(file.model_dump(exclude={"id"}))},
+    )
---
+++ study_buddy/repositories/flashcard_repository.py
@@ -0,0 +1,24 @@
+import json
+
+from study_buddy.models.flashcards import Flashcards, Flashcard
+from study_buddy.utils.database import flashcard_collection
+
+
+def save_many(flashcards: Flashcards, course_id: str):
+    saved_questions = flashcard_collection.insert_many(
+        [
+            json.loads(
+                Flashcard(
+                    back=flashcard.back, front=flashcard.front, course_id=course_id
+                ).model_dump(exclude={"id"})
+            )
+            for flashcard in flashcards.flash_cards
+        ]
+    )
+    return str(saved_questions.inserted_ids)
+
+
+def find_by_course(course_id: str) -> list[Flashcard]:
+    raw_flashcards = flashcard_collection.find({"course_id": course_id})
+    flashcards = [Flashcard(**raw_flashcard) for raw_flashcard in raw_flashcards]
+    return flashcards
