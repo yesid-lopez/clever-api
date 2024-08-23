0d519b7fdb8a08e5246575b6a2d1289926aa6cbf
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2024-08-23 17:21:44 +0200

    chore: Update trulens-eval dependency to version 0.33.0

--- study_buddy/utils/trulens.py
+++ study_buddy/utils/trulens.py
@@ -1,44 +1,42 @@
 import os
 
 import numpy as np
-from trulens_eval import Feedback, Select, Tru, TruLlama
+from trulens_eval import Feedback, Tru, TruLlama
 from trulens_eval.feedback.provider import OpenAI
+from trulens_eval.app import App
 
 
 class TrulensClient:
     def __init__(self):
         self.tru = Tru(database_url=os.getenv("TRULENS_DB_URI"))
-        self.initialize_feedback_functions()
 
-    def initialize_feedback_functions(self):
-        provider = OpenAI()
+    def initialize_feedback_functions(self, query_engine):
+        provider = OpenAI(model_engine="gpt-4o")
+        context = App.select_context(query_engine)
         self.f_groundedness = (
             Feedback(
                 provider.groundedness_measure_with_cot_reasons,
                 name="Groundedness",
             )
-            # collect context chunks into a list
-            .on(Select.RecordCalls.retrieve.rets.collect()).on_output()
-        )
-
-        self.f_answer_relevance = (
-            Feedback(
-                provider.relevance_with_cot_reasons, name="Answer Relevance"
-            )
-            .on_input()
+            .on(context.collect())  # collect context chunks into a list
             .on_output()
         )
+
+        self.f_answer_relevance = Feedback(
+            provider.relevance_with_cot_reasons, name="Answer Relevance"
+        ).on_input_output()
         self.f_context_relevance = (
             Feedback(
                 provider.context_relevance_with_cot_reasons,
                 name="Context Relevance",
             )
             .on_input()
-            .on(Select.RecordCalls.retrieve.rets[:])
+            .on(context)
             .aggregate(np.mean)
         )
 
     def create_recorder(self, query_engine, app_id: str):
+        self.initialize_feedback_functions(query_engine)
         tru_query_engine_recorder = TruLlama(
             query_engine,
             app_id=app_id,
